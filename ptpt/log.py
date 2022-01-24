6135a8ab0692f9e3c36904da5f0309ae740cf93e
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2022-01-24 16:57:15 +0000

    adds rank0 wrapping of printing functions

--- ptpt/log.py
+++ ptpt/log.py
@@ -19,3 +19,14 @@ info = logger.info
 warning = logger.warning
 error = logger.error
 critical = logger.critical
+
+def wrap_log(accelerator):
+    global debug, info, warning, error, critical
+    noop = lambda *args, **kwargs: None
+    wrapping_fn = lambda f: f if accelerator.is_local_main_process else noop
+
+    debug = wrapping_fn(debug)
+    info = wrapping_fn(info)
+    warning = wrapping_fn(warning)
+    error = wrapping_fn(error)
+    critical = wrapping_fn(critical)
